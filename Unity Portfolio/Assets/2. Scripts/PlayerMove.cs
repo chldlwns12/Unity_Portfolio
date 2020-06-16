f1d8af6e3a643d546e3b595db9ecb2651530e811
chldlwns12 <chldlwns12@gmail.com>
2020-06-16 17:44:06 +0900

    2020.06.16 유니티 프로젝트 생성 버전 : 2019.3.14f1(64-bit)
    -캐릭터 생성 및 이동

---
+++ Unity Portfolio/Assets/2. Scripts/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public static PlayerMove Instance;
+    private void Awake()
+    {
+        if(Instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    Rigidbody rb;
+    public float playerSpeed = 25.0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Debug.Log("moveHorizontal : " + moveHorizontal + " / moveVertical : " + moveVertical);
+
+        Vector3 dir = new Vector3(moveHorizontal, 0, moveVertical);
+
+        transform.Translate(dir * playerSpeed * Time.deltaTime);
+    }
+}
