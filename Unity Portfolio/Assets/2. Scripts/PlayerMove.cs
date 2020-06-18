6584622dde8a447e344eb59b00e054d03ee7a3fb
chldlwns12 <chldlwns12@gmail.com>
2020-06-18 13:53:36 +0900

    2020.06.18 -캐릭터 생성 및 조이스틱을 이용한 이동 구현

--- Unity Portfolio/Assets/2. Scripts/PlayerMove.cs
+++ Unity Portfolio/Assets/2. Scripts/PlayerMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    Rigidbody rb;
+    public float playerSpeed = 15.0f;
+    public VariableJoystick joystick;
 
     public static PlayerMove Instance;
     private void Awake()
@@ -18,9 +21,6 @@ public class PlayerMove : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    Rigidbody rb;
-    public float playerSpeed = 25.0f;
-
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -31,10 +31,21 @@ public class PlayerMove : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        if (moveHorizontal == 0 && moveVertical == 0)
+        {
+            moveHorizontal = joystick.Horizontal;
+            moveVertical = joystick.Vertical;
+        }
+
         Debug.Log("moveHorizontal : " + moveHorizontal + " / moveVertical : " + moveVertical);
 
         Vector3 dir = new Vector3(moveHorizontal, 0, moveVertical);
 
         transform.Translate(dir * playerSpeed * Time.deltaTime);
+
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(position.x, -2.5f, 2.6f);
+        position.z = Mathf.Clamp(position.z, -7f, 6.8f);
+        transform.position = position;
     }
 }
